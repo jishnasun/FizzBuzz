fd19da0b4b61bc5d8ef44610f188ef0c291cfa07
J Sun <jishna.sunkara@gmail.com>
2023-09-11 12:38:23 -0400

    updated README, FizzBuzz.java, and added new file Main.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,7 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main() {
 
         //for (int i = 1; i < 100; i++) {
             // for loop: i is an integer that equals 1, and increases by 1 each iteration
@@ -12,7 +12,6 @@ class FizzBuzz {
 
         while (i < 100) {
 
-            // Find out which numbers divide i.
             doFizzBuzz(i);
 
             i++;
@@ -20,7 +19,8 @@ class FizzBuzz {
         }
     }
 
-    private static void doFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
+        // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;  // seem to define what type of object the variable
         boolean divisibleBy5 = i % 5 == 0;  // has before variable name in order to define it
 
